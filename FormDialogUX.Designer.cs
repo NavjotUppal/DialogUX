d498d88b590ab63a4c78ff7ff59feebe17b3b128
NAVJOT KAUR <uppal.navjot25@gmail.com>
2022-08-10 10:32:39 +0800

    Add project files.

---
+++ FormDialogUX.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace DialogUX
+{
+    partial class FormDialogUX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxTelephone = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.ItemHeight = 16;
+            this.listBoxOutput.Location = new System.Drawing.Point(43, 29);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(374, 244);
+            this.listBoxOutput.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(543, 46);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(543, 115);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTitle.TabIndex = 2;
+            // 
+            // textBoxTelephone
+            // 
+            this.textBoxTelephone.Location = new System.Drawing.Point(543, 174);
+            this.textBoxTelephone.Name = "textBoxTelephone";
+            this.textBoxTelephone.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTelephone.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(515, 221);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonAdd_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(498, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(483, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Job Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(469, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telephone";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(467, 304);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(593, 304);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 9;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // FormDialogUX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxTelephone);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.listBoxOutput);
+            this.Name = "FormDialogUX";
+            this.Text = "My Dialog Box Example";
+            this.Load += new System.EventHandler(this.FormDialogUX_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxTelephone;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ FormDialogUX.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// 10/08/2022
+//Dialog box Example
+namespace DialogUX
+{
+    public partial class FormDialogUX : Form
+    {
+        public FormDialogUX()
+        {
+            InitializeComponent();
+        }
+        static int rowSize = 8;
+        static int columnSize = 3;
+        string[,] employees=new string[rowSize, columnSize];
+        string defaultFileName = "default.bin";
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "bin file|*.bin";
+            saveFileDialog.Title = "Save A BIN File";
+            saveFileDialog.InitialDirectory = Application.StartupPath;
+            saveFileDialog.DefaultExt = "bin";
+            saveFileDialog.ShowDialog();
+            string fileName=saveFileDialog.FileName;
+            if(saveFileDialog.FileName !="")
+            {
+                SaveRecord(fileName);
+            }
+            else
+            {
+                SaveRecord(defaultFileName);
+            }
+        }
+        private void SaveRecord(String saveFileName)
+        {
+            try 
+            {
+                using (Stream stream = File.Open(saveFileName,FileMode.Create))
+                {
+                    BinaryFormatter bin=new BinaryFormatter();
+                    {
+                        for(int y=0; y<columnSize; y++)
+                        {
+                            for(int x=0; x<rowSize; x++)
+                            {
+                                bin.Serialize(stream, employees[x, y]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonAdd_MouseClick(object sender, MouseEventArgs e)
+        {
+            // no error trapping
+            for(int x=0;x<rowSize;x++)
+            {
+                if (employees[x, 0] == "~")
+                {
+                    employees[x, 0] = textBoxName.Text;
+                    employees[x, 1] = textBoxTitle.Text;
+                    employees[x, 2] = textBoxTelephone.Text;
+
+                    var result = MessageBox.Show("Proceed with new Record?", "Add new Record",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                        break;
+                    else
+                    {
+                        employees[x, 0] = "~";  //Name
+                        employees[x, 1] = "Sales";   //Job Title
+                        employees[x, 2] = "1234451";   //Telephone
+                        break;
+
+                    }
+
+                }
+                else
+                { 
+                    MessageBox.Show("The array is full", "Array is FULL");
+                }
+            }
+            DisplayEmployees();
+        }
+
+        private void DisplayEmployees()
+        {
+            listBoxOutput.Items.Clear();
+            for (int x = 0; x < rowSize; x++)
+            {
+                listBoxOutput.Items.Add(employees[x,0]+
+                    " "+employees[x,1]+
+                    " "+employees[x,2]);
+            }
+        }
+        private void InitialiseArray()
+        {
+            for (int x = 0; x < rowSize; x++)
+            {
+                employees[x, 0] = "~";  //Name
+                employees[x, 1] = "";   //Job Title
+                employees[x, 2] = "";   //Telephone
+            }
+            DisplayEmployees();
+        }
+
+        private void FormDialogUX_Load(object sender, EventArgs e)
+        {
+            InitialiseArray();
+        }
+
+        
+    }
+}
